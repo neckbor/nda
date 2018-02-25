e21ce7f4f40168c7c57f4a07bcee5899a2ccf4d2
neckbor <neckbor@mail.ru>
2018-02-26 02:05:20 +0300

    создал проект;

---
+++ 8.13/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace _8._13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mtxGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.mtxGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mtxGrid
+            // 
+            this.mtxGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.mtxGrid.Location = new System.Drawing.Point(13, 13);
+            this.mtxGrid.Name = "mtxGrid";
+            this.mtxGrid.RowTemplate.Height = 24;
+            this.mtxGrid.Size = new System.Drawing.Size(240, 150);
+            this.mtxGrid.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 352);
+            this.Controls.Add(this.mtxGrid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.mtxGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView mtxGrid;
+    }
+}
+
