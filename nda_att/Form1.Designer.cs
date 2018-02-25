765cabf6335bfde1b0552489efcc02f91ef9dfc7
neckbor <neckbor@mail.ru>
2018-02-26 01:04:09 +0300

    реализована идентификация одинаковых элементов; начал делать конструктор для перезаписи массива;

---
+++ nda_att/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace nda_att
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.runBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.resultText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // inputText
+            // 
+            this.inputText.Location = new System.Drawing.Point(12, 46);
+            this.inputText.Name = "inputText";
+            this.inputText.Size = new System.Drawing.Size(461, 22);
+            this.inputText.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите массив чисел";
+            // 
+            // runBtn
+            // 
+            this.runBtn.Location = new System.Drawing.Point(129, 74);
+            this.runBtn.Name = "runBtn";
+            this.runBtn.Size = new System.Drawing.Size(211, 43);
+            this.runBtn.TabIndex = 2;
+            this.runBtn.Text = "Убрать повторяющиеся элементы";
+            this.runBtn.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Результат";
+            // 
+            // resultText
+            // 
+            this.resultText.Location = new System.Drawing.Point(12, 169);
+            this.resultText.Name = "resultText";
+            this.resultText.Size = new System.Drawing.Size(461, 22);
+            this.resultText.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 240);
+            this.Controls.Add(this.resultText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.runBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button runBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox resultText;
+    }
+}
+
---
+++ nda_att/SameElementsDelete.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nda_att
+{
+    public class SameElementsDelete
+    {
+        private int[] _data;
+        private string result;
+        public SameElementsDelete(string input)
+        {
+            _data = StrToArray<int>(input);
+        }
+
+        private void RefreshData()
+        { }
+
+        // Функция конвертирует строку в массив элементов T
+        // (при невозможности конвертации происходит ошибка)
+        private T[] StrToArray<T>(string str)
+        {
+            return Array.ConvertAll(
+                str.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries),
+                (s) => StrToValue<T>(s)
+            );
+        }
+
+        // Функция конвертирует строку в значение T
+        // (при невозможности конвертации происходит ошибка)
+        private T StrToValue<T>(string str)
+        {
+            return (T)Convert.ChangeType(str, typeof(T));
+        }
+
+        //Перебор массива по элементам и вызов удаляющуго метода
+        public void SameItemsDetect()
+        {
+            for (int i = 0; i < _data.Length; i++)
+                IfSame(i);
+        }
+
+        //Удаление одинаковых элементов с данным
+        private void IfSame(int i)
+        {
+            for (int ind = i + 1; ind < _data.Length; ind++)
+                if (_data[ind] == _data[i])
+                    Remove(ind);
+        }
+
+        private void Remove(int ind)
+        {
+            for (; ind < _data.Length; ind++)
+                if (ind != _data.Length - 1)
+                    _data[ind] = _data[ind + 1];
+                else
+                    _data[ind] = ;
+        }
+
+    }
+
+}
