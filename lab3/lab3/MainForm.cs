6e48c9d35cc3e9a033aae89676ef477e163b9c94
neckbor <neckbor@mail.ru>
2018-10-29 22:50:30 +0300

    отрисовка через setpixel;

--- lab3/lab3/MainForm.cs
+++ lab3/lab3/MainForm.cs
@@ -26,62 +26,154 @@ namespace lab3
             
             bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             g = Graphics.FromImage(bitmap);
-            //g.Clear(Color.White);
 
             double angle = (n - 2) * 180 / n;
             angle = (Math.PI * angle) / 180;
-            //int a = 70;
 
-            PointF centr = new PointF(200, 200);
+            Point centr = new Point(200, 200);
             double R = 100;
 
-            //PointF end = DrawEdge(start, a, angle);
-
-            PointF p1 = GetVertwex(centr, R, n, 0);
-            PointF p2;
+            Point p1 = GetVertwex(centr, R, n, 0);
+            Point p2;
 
             for (int i = 1; i <= n; i++)
             {
                 p2 = GetVertwex(centr, R, n, i);
 
-                //g.DrawLine(Pens.Black, p1, p2);
                 //DrawLineDDA(p1, p2);
+                //Bresenham(p1, p2);
+                DrawLineHabr(p1, p2);
 
                 p1 = p2;
             }
         }
 
         //высчитывет вершину
-        private PointF GetVertwex(PointF pointCentr, double R, int n, int i)
+        private Point GetVertwex(Point pointCentr, double R, int n, int i)
         {
-            PointF p = new PointF();
+            Point p = new Point();
 
-            p.X = (float)(pointCentr.X + R * Math.Cos(2 * Math.PI * i / n));
-            p.Y = (float)(pointCentr.Y + R * Math.Sin(2 * Math.PI * i / n));
+            double x = pointCentr.X + R * Math.Cos(2 * Math.PI * i / n);
+            double y = pointCentr.Y + R * Math.Sin(2 * Math.PI * i / n);
+
+            p.X = (int)Math.Round(x, MidpointRounding.AwayFromZero);
+            p.Y = (int)Math.Round(y, MidpointRounding.AwayFromZero);
 
             return p;
         }
 
         //алгоритм для рисования прямых
-        private void DrawLineDDA(PointF start, PointF end)
+        private void DrawLineDDA(Point start, Point end)
         {
             double L = Math.Max(Math.Abs(end.X - start.X), Math.Abs(end.Y - start.Y));
-            double dx = (end.X - start.X) / L;
-            double dy = (end.Y - start.Y) / L;
-            double x = start.X;
-            double y = start.Y;
+            int dx = (int)Math.Round((end.X - start.X) / L, MidpointRounding.AwayFromZero);
+            int dy = (int)Math.Round((end.Y - start.Y) / L, MidpointRounding.AwayFromZero);
+            int x = start.X;
+            int y = start.Y;
 
             for (int i = 0; i <= L; i++)
             {
                 x += dx;
                 y += dy;
 
-                x = Math.Round(x, 0);
-                y = Math.Round(y, 0);
-
-                bitmap.SetPixel((int)x, (int)y, Color.Black);
+                bitmap.SetPixel(x, y, Color.Black);
                 
             }
+        }
+
+        private void Bresenham(Point start, Point end)
+        {
+            //int x = start.X;
+            //int y = start.Y;
+            //int Dx = end.X - start.X;
+            //int Dy = end.Y - start.Y;
+            //int e = 2 * Dy - Dx;
+            //for (int i = 1; i <= Dx; i++)
+            //{
+            //    bitmap.SetPixel(x, y, Color.Black);
+            //    if (e >= 0)
+            //    {
+            //        y++;
+            //        e += -2 * Dx + 2 * Dy;
+            //    }
+            //    else
+            //        e += 2 * Dy;
+            //    x++;
+            //}
+
+            int dx = Math.Abs(end.X - start.X);
+            int dy = Math.Abs(end.Y - start.Y);
+            int e = 0;
+            int de = dy;
+            int y = start.Y;
+            int diry = end.Y - start.Y;
+
+            if (diry > 0)
+                diry = 1;
+            if (diry < 0)
+                diry = -1;
+            for(int x = Math.Min(start.X, end.X); x <= Math.Max(start.X, end.X); x++)
+            {
+                bitmap.SetPixel(x, y, Color.Black);
+                e += de;
+                if (2 * e >= dx)
+                {
+                    y += dy;
+                    e -= de;
+                }
+            }
+        }
+
+        private void DrawLineHabr(Point start, Point end)
+        {
+            bool flag = false;
+            if(Math.Abs(start.X - end.X) < Math.Abs(start.Y - end.Y))
+            {
+                Interpolate(ref start, ref end);
+                flag = true;
+            }
+
+            if (start.X > end.X)
+            {
+                Swap(ref start, ref end);
+            }
+
+            float a = (float)(end.Y - start.Y) / (float)(end.X - start.X);
+            double y = start.Y;
+
+            for (int x = start.X; x <= end.X; x++)
+            {
+                int yy = (int)Math.Round(y, MidpointRounding.AwayFromZero);
+
+                if(flag)
+                    bitmap.SetPixel(yy, x, Color.Black);
+                else
+                    bitmap.SetPixel(x, yy, Color.Black);
+
+                y += a;
+            }
+        }
+
+        private void Swap(ref Point start, ref Point end)
+        {
+            int a = start.X;
+            start.X = end.X;
+            end.X = a;
+
+            a = start.Y;
+            start.Y = end.Y;
+            end.Y = a;
+        }
+
+        private void Interpolate(ref Point start, ref Point end)
+        {
+            int a = start.X;
+            start.X = start.Y;
+            start.Y = a;
+
+            a = end.X;
+            end.X = end.Y;
+            end.Y = a;
         }
 
         private void buttonDraw_Click(object sender, EventArgs e)
