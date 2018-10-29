3c2977282e027f64b3be166c688a4de4439d02ef
neckbor <neckbor@mail.ru>
2018-10-29 18:23:13 +0300

    проект 3 лабы по кг; сделано построение многоугольника, при методе setpixel отрисовывает плохо;

---
+++ lab3/lab3/MainForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace lab3
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxR = new System.Windows.Forms.TextBox();
+            this.textBoxN = new System.Windows.Forms.TextBox();
+            this.buttonDraw = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxR
+            // 
+            this.textBoxR.Location = new System.Drawing.Point(427, 13);
+            this.textBoxR.Name = "textBoxR";
+            this.textBoxR.Size = new System.Drawing.Size(47, 20);
+            this.textBoxR.TabIndex = 0;
+            // 
+            // textBoxN
+            // 
+            this.textBoxN.Location = new System.Drawing.Point(427, 39);
+            this.textBoxN.Name = "textBoxN";
+            this.textBoxN.Size = new System.Drawing.Size(47, 20);
+            this.textBoxN.TabIndex = 1;
+            // 
+            // buttonDraw
+            // 
+            this.buttonDraw.Location = new System.Drawing.Point(406, 65);
+            this.buttonDraw.Name = "buttonDraw";
+            this.buttonDraw.Size = new System.Drawing.Size(68, 23);
+            this.buttonDraw.TabIndex = 2;
+            this.buttonDraw.Text = "Draw";
+            this.buttonDraw.UseVisualStyleBackColor = true;
+            this.buttonDraw.Click += new System.EventHandler(this.buttonDraw_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(403, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "R:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(404, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "N:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(385, 371);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 388);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonDraw);
+            this.Controls.Add(this.textBoxN);
+            this.Controls.Add(this.textBoxR);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.PaintForm);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxR;
+        private System.Windows.Forms.TextBox textBoxN;
+        private System.Windows.Forms.Button buttonDraw;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ lab3/lab3/MainForm.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab3
+{
+    public partial class MainForm : Form
+    {
+        Graphics g;
+        Bitmap bitmap;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        //рисует многоугольник
+        private void DrawRectangle(double r, int n)
+        {
+            
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bitmap);
+            //g.Clear(Color.White);
+
+            double angle = (n - 2) * 180 / n;
+            angle = (Math.PI * angle) / 180;
+            //int a = 70;
+
+            PointF centr = new PointF(200, 200);
+            double R = 100;
+
+            //PointF end = DrawEdge(start, a, angle);
+
+            PointF p1 = GetVertwex(centr, R, n, 0);
+            PointF p2;
+
+            for (int i = 1; i <= n; i++)
+            {
+                p2 = GetVertwex(centr, R, n, i);
+
+                //g.DrawLine(Pens.Black, p1, p2);
+                //DrawLineDDA(p1, p2);
+
+                p1 = p2;
+            }
+        }
+
+        //высчитывет вершину
+        private PointF GetVertwex(PointF pointCentr, double R, int n, int i)
+        {
+            PointF p = new PointF();
+
+            p.X = (float)(pointCentr.X + R * Math.Cos(2 * Math.PI * i / n));
+            p.Y = (float)(pointCentr.Y + R * Math.Sin(2 * Math.PI * i / n));
+
+            return p;
+        }
+
+        //алгоритм для рисования прямых
+        private void DrawLineDDA(PointF start, PointF end)
+        {
+            double L = Math.Max(Math.Abs(end.X - start.X), Math.Abs(end.Y - start.Y));
+            double dx = (end.X - start.X) / L;
+            double dy = (end.Y - start.Y) / L;
+            double x = start.X;
+            double y = start.Y;
+
+            for (int i = 0; i <= L; i++)
+            {
+                x += dx;
+                y += dy;
+
+                x = Math.Round(x, 0);
+                y = Math.Round(y, 0);
+
+                bitmap.SetPixel((int)x, (int)y, Color.Black);
+                
+            }
+        }
+
+        private void buttonDraw_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DrawRectangle(double.Parse(textBoxR.Text), int.Parse(textBoxN.Text));
+
+                pictureBox1.Image = bitmap;
+                //g.DrawLine(Pens.Black, 100, 100, 103, 109);
+                //g.DrawLine(Pens.Black, 103, 109, 106, 119);
+                //g.DrawLine(Pens.Black, 106, 119, 109, 128);
+                //g.DrawLine(Pens.Black, 109, 128, 112, 138);
+                //g.DrawLine(Pens.Black, 112, 138, 115, 147);
+
+            }
+            catch(Exception ex)
+            {
+                textBoxN.Text = "";
+                textBoxR.Text = "";
+            }
+
+        }
+
+
+        private void PaintForm(object sender, PaintEventArgs e)
+        {
+            try
+            {
+                DrawRectangle(double.Parse(textBoxR.Text), int.Parse(textBoxN.Text));
+
+                pictureBox1.Image = bitmap;
+            }
+            catch (Exception ex)
+            { }
+        }
+
+        //protected override void OnPaint(PaintEventArgs e)
+        //{
+        //    base.OnPaint(e);
+
+        //    try
+        //    {
+        //        Graphics g = this.CreateGraphics();
+        //        DrawRectangle(1, 5);
+
+        //        g.DrawLine(Pens.Black, 200, 200, 200, 230);
+        //        g.DrawLine(Pens.Black, 200, 200, 240, 260);
+
+        //        //g = Graphics.FromImage(bitmap);
+        //        g.Dispose();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        textBoxN.Text = "";
+        //        textBoxR.Text = "";
+        //    }
+        //}
+
+        //private PointF DrawEdge(PointF start, int a, double angle)
+        //{
+        //    float dx = (float)(a * Math.Cos(angle));
+        //    float dy = (float)(a * Math.Sin(angle));
+        //    PointF end = new PointF(start.X + dx, start.Y + dy);
+
+
+
+        //    g.DrawLine(Pens.Black, start, end);
+        //    //DrawLineDDA(start, end);
+        //    //gr = Graphics.FromImage(bitmap);
+        //    //gr.Clear(Color.White);
+
+        //    return end;
+        //}
+    }
+}
