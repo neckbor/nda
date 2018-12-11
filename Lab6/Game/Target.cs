eee4cf113d03ee88a9560ff22595f4f7a80085cc
neckbor <neckbor@mail.ru>
2018-12-11 22:27:48 +0300

    начал реализацию класса цели;

--- Lab6/Game/Target.cs
+++ Lab6/Game/Target.cs
@@ -12,11 +12,45 @@ namespace Game
         public int Y { get; set; }
         public int Distance { get; }
 
-        private double _a;
+        private Path _path;
+
+        private enum Path
+        {
+            STRAIGHT,
+            TO_TOP,
+            TO_BOTTOM,
+            HILL
+        };
+          
+
 
         public Target()
         {
+            Random rnd = new Random();
+            int path = rnd.Next(0, 3);
 
+            //определяю по какой траектроии пойдёт цель и с какой стороны
+            switch (path)
+            {
+                case 0:
+                    _path = Path.STRAIGHT;
+                    X = 0;
+                    break;
+                case 1:
+                    _path = Path.TO_TOP;
+                    X = 1400;
+                    break;
+                case 2:
+                    _path = Path.TO_BOTTOM;
+                    X = 0;
+                    break;
+                case 3:
+                    _path = Path.HILL;
+                    X = 1400;
+                    break;
+            }
+            
+            
         }
 
         public void Move()
--- Lab6/Lab6/MainScreen.Designer.cs
+++ Lab6/Lab6/MainScreen.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1353, 891);
+            this.ClientSize = new System.Drawing.Size(1384, 891);
             this.Name = "MainScreen";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.MainScreen_Load);
